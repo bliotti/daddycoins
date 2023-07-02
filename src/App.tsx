2ae81c5e83bb8707b74d03f104b1df86adae14f0
Brian Liotti <bliotti@protonmail.com>
2023-07-02 15:16:03 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          daddycoins
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Coming Soon
+        </h2>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
